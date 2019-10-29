9a31075f26fb38486a49c1410568c73b1027a14a
João Goya <joaogoya@gmail.com>
2019-10-29 17:21:25 -0300

    Table searchbox

--- front-end/src/app/shared/data-table/data-table.component.ts
+++ front-end/src/app/shared/data-table/data-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-data-table',
@@ -16,16 +16,49 @@ export class DataTableComponent implements OnInit {
   public page = 1;
   public pageSize = 10;
 
+  /*search box*/
+  public term = '';
+  public hasResult = false;
+  public search = false;
+
   constructor() { }
 
   ngOnInit() {
-    this.dataHandlig(this.infos.data);
+    this.dataHandlig();
   }
 
-  public dataHandlig(infos) {
-      this.atributes = Object.keys(infos[0]);
-      infos.forEach(element => {
+  public dataHandlig() {
+      this.atributes = Object.keys(this.infos.data[0]);
+      this.infos.data.forEach(element => {
         this.data.push(Object.values(element));
       });
   }
+
+  public searchBar(term) {
+    this.search = true;
+    this.term = term;
+    const arrayTemp  = [];
+    this.data.forEach(item => {
+      item.forEach(element => {
+        if(element.toString().includes(term)) {
+          arrayTemp.push(item);
+        }
+      });
+    });
+    if (arrayTemp.length > 0) {
+      this.data = [];
+      this.data = arrayTemp;
+      this.hasResult = true;
+    } else {
+      this.data = [];
+      this.hasResult = false;
+    }
+  }
+
+  public reload(){
+    this.hasResult = true;
+    this.dataHandlig();
+  }
+
+
 }
