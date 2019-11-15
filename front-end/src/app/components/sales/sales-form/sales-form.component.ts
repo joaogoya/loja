90b8c885c68f0c8eb48243af5e358ed7f338479d
João Goya <joaogoya@gmail.com>
2019-11-15 10:53:45 -0300

    sales html and add products

--- front-end/src/app/components/sales/sales-form/sales-form.component.ts
+++ front-end/src/app/components/sales/sales-form/sales-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientsService } from 'src/app/services/clients/clients-.service';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-sales-form',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalesFormComponent implements OnInit {
 
-  constructor() { }
+  public allClients = [];
+  public allProducts = [];
+  public addedProducots = [];
+
+  constructor(
+    private clientsService: ClientsService,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit() {
+    this.getAllClients();
+    this.getAllProducts();
+  }
+  public getAllClients() {
+    this.clientsService.getAll().subscribe(clients => this.allClients = clients);
   }
 
+  public getAllProducts() {
+    this.productsService.getAll().subscribe(products => this.allProducts = products);
+  }
+
+  public addProduct(product) {
+    this.addedProducots.push(product);
+    this.allProducts = this.allProducts.filter(( obj ) => {
+      return obj._id !== product._id;
+    });
+  }
+
+  public removeProduct(product) {
+    this.allProducts.push(product);
+    this.addedProducots = this.addedProducots.filter(( obj ) => {
+      return obj._id !== product._id;
+    });
+  }
 }
